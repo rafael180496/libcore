8c5b2f7ca3a61d603c2db7aa47b68fc8bf3f9a11
rafael180496 <rafael180496@gmail.com>
2021-03-02 10:14:05 -0600

    Creacion de senformat para apis rest echo

--- server/msjserver.go
+++ server/msjserver.go
@@ -1,6 +1,10 @@
 package server
 
-import echo "github.com/labstack/echo/v4"
+import (
+	"fmt"
+
+	echo "github.com/labstack/echo/v4"
+)
 
 type (
 	/*MapMsjPet : maneja el envio de mensajes con peticiones echo*/
@@ -21,6 +25,19 @@ func (p *MapMsjPet) Send(cod string, data interface{}, e echo.Context) error {
 	petmsj := maps[cod]
 	datainfo.Code = petmsj.Code
 	datainfo.Error = petmsj.Msj
+	datainfo.Data = data
+	return datainfo.ResultJSON(e)
+}
+
+/*SendFormat : envia un mensaje de la peticiones pre-cargada con un formato de mensaje */
+func (p *MapMsjPet) SendFormat(cod string, data interface{}, e echo.Context, param ...interface{}) error {
+	var (
+		datainfo StDataEnv
+	)
+	maps := *p
+	petmsj := maps[cod]
+	datainfo.Code = petmsj.Code
+	datainfo.Error = fmt.Sprintf(petmsj.Msj, param...)
 	datainfo.Data = data
 	return datainfo.ResultJSON(e)
 }
